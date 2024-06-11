a97ecd91bf1e9f26f505b8bf9d6e1383b4335101
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-11 14:43:43 +0530

    implement infinite scrolling

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash Api
 const count = 30;
 const apiKey = 'zqaZmCNyu3HijvJ6rVsVZi6r0DlTJ2Sfj9Uq5lVfGtU';
-const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Check if all images are loaded
+function imageLoaded() {
+  imagesLoaded++;
+  console.log(imagesLoaded);
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper function to Set attribute on dom elements
 function setAttributes(element, attribute) {
@@ -17,6 +30,8 @@ function setAttributes(element, attribute) {
 
 // Create elements for links and photos,add to dom
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run function for each object in photosArray
   photosArray.forEach(photo => {
     // Create link to full pohoto
@@ -33,6 +48,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // To check if each image is loaded
+    img.addEventListener('load', imageLoaded);
 
     // Put <img> inside <a>, then put both in imagecontainer
     item.appendChild(img);
@@ -51,6 +68,17 @@ async function getPhotos() {
     // Catch error here
   }
 }
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (
+    (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) &
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On load
 getPhotos();
